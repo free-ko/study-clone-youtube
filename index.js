2acf57965e0333ee055f384cc9fb65d0e1842c9e
kyw <[duddnr654@naver.com]>
2020-06-18 16:40:52 +0900

    2.3 Set : package.json / index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hi");
+const express = require('express'); // require은 node_module을 어딘가에서 가져오는 것, 지금은 express라는 파일을 찾고 있는 것 몿 장으면 node modules에서 express를 찾음, 결국 express를 import 한거임
+const app = express(); // application을 만드는 것 ; app에 express를 싱행해서 담은 것
+
+const PORT = 4000;
+
+function handleListening() {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);  // 4000포트를 listen하기 시작할 때 handleListening 함수를 콜백해줘
