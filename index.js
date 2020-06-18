e82c5ac26fe8473e9ef685dcf5f2949665eb0ab6
kyw <duddnr654@naver.com>
2020-06-18 22:00:32 +0900

    2.7 Express Core : Middlewares part Two
    - Morgan
    - Helmet
    - Cookie parser
    - Body parser

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -10,14 +15,13 @@ const handleHome = (req, res) => res.send("Hi from home!");
 
 const handleProfile = (req, res) => res.send("You ar on my dream");
 
-const betweenHome = (req, res, next) => {
-  console.log("Between");
-  next();
-};
+app.use(cookieParser());
+app.use(bodyParser.json()); // 마찬가지로 json도 이해하기 위해서 유저가 보낸 데이터를
+app.use(bodyParser.urlencoded({ extended: true })); // 유저가 보낸 form에서 받은 데이터를 이해하기위해
+app.use(helmet());
+app.use(morgan("dev")); //나의 첫 middleware임
 
-app.use(betweenHome);
-
-app.get("/", handleHome); // 함수는 request object, response object을 요청함
+app.get("/", middleware, handleHome); // 함수는 request object, response object을 요청함
 
 app.get("/profile", handleProfile);
 
