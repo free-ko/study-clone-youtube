668b1f13cde048686136c46e8d32c8662bc618af
free-ko <[duddnr654@naver.com]>
2020-06-18 18:23:25 +0900

    2.5 ES6 on Node JS using Babel
    - nodemon -D install
    - @babe/core, node, preset-env
    - start reset
    - arrow function
    - create babelrc

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-const express = require('express'); // require은 node_module을 어딘가에서 가져오는 것, 지금은 express라는 파일을 찾고 있는 것 몿 장으면 node modules에서 express를 찾음, 결국 express를 import 한거임
-const app = express(); // application을 만드는 것 ; app에 express를 싱행해서 담은 것
+import express from "express";
+const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
     console.log(`Listening on: http://localhost:${PORT}`);
-}
 
-function handleHome(req, res) {
-    res.send('Hi from home!');
-}
+const handleHome = (req, res) => res.send('Hi from home!');
 
-function handleProfile(req, res) {
-    res.send("You ar on my frofile");
-}
+const handleProfile = (req, res) =>  res.send("You ar on my dream");
 
 app.get("/", handleHome); // 함수는 request object, response object을 요청함
 
