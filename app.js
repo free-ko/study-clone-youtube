f2bc3bd2c33dcf2d3c70c91c901060d7254a92b5
kyw <duddnr654@naver.com>
2020-06-19 11:59:15 +0900

    2.8 Express Core: Routing
    - Calling file : init.js / app.js
    - Router meaing
    - Router.js use to app.js

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import {
+  userRouter
+} from "./router";
+
 
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hi from home!");
 
@@ -17,12 +16,18 @@ const handleProfile = (req, res) => res.send("You ar on my dream");
 
 app.use(cookieParser());
 app.use(bodyParser.json()); // 마찬가지로 json도 이해하기 위해서 유저가 보낸 데이터를
-app.use(bodyParser.urlencoded({ extended: true })); // 유저가 보낸 form에서 받은 데이터를 이해하기위해
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+); // 유저가 보낸 form에서 받은 데이터를 이해하기위해
 app.use(helmet());
 app.use(morgan("dev")); //나의 첫 middleware임
 
-app.get("/", middleware, handleHome); // 함수는 request object, response object을 요청함
+app.get("/", handleHome); // 함수는 request object, response object을 요청함
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening); // 4000포트를 listen하기 시작할 때 handleListening 함수를 콜백해줘
+app.use("/user", userRouter);
+
+export default app;
