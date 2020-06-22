0a8089868337097bfcfd923572fbb0133ca0563b
kyw <duddnr654@naver.com>
2020-06-22 21:27:55 +0900

    2.25 More Controlers
    - 비디오 클릭시 상세페이지 작업
    - 로그아웃 할시 홈으로
    - 업로드 주소 재설정
    -업로드 후에 비디오 상세페이지로
    - 아무것도 입력안할시 상세페이지로 넘어가는 거 방지

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,44 @@
 import routes from "../routes";
 
 export const getJoin = (req, res) => {
-    res.render("Join", {pageTitle: "Join"});
+    res.render("Join", {
+        pageTitle: "Join"
+    });
 };
 
 export const postJoin = (req, res) => {
     const {
-        body: {name, email, password, password2}
+        body: {
+            name,
+            email,
+            password,
+            password2
+        }
     } = req;
-    if(password !== password2){
+    if (password !== password2) {
         res.status(400);
-        res.render("join", {pageTitle: "Join"});
-    } else{
+        res.render("join", {
+            pageTitle: "Join"
+        });
+    } else {
         // To Do: Register User
         // To Do: Log user in
         res.redirect(routes.home);
     }
 };
 
-export const getLogin = (req, res) => res.render("login", {pageTitle: "Log in"});
+export const getLogin = (req, res) => res.render("login", {
+    pageTitle: "Log in"
+});
 export const postLogin = (req, res) => {
     res.redirect(routes.home);
 };
 
-export const logout = (req, res) => res.render("logout");
+export const logout = (req, res) => {
+    // To Do: Process Log Out
+    res.redirect(routes.home);
+};
+
 export const userDetail = (req, res) => res.render("userDetail");
 export const editProfile = (req, res) => res.render("editProfile");
 export const changePassword = (req, res) => res.render("changePassword");
