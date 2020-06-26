302cdd02a0514d704ccf04f625a182c4cb9810d7
kyw <[duddnr654@naver.com]>
2020-06-26 15:07:17 +0900

    3.6 Uploading and Creating Video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -36,16 +36,18 @@ export const getUpload = (req, res) =>
         pageTitle: "Upload"
     });
 
-export const postUpload = (req, res) => {
+export const postUpload = async(req, res) => {
     const {
-        body: {
-            file,
-            title,
-            description
-        }
+        body: { title, description },
+        file: { path }
     } = req;
-    // To Do: Upload and save video
-    res.redirect(routes.videoDetail(324393));
+    const newVideo = await Video.create({
+        fileUrl: path,
+        title,
+        description
+    });
+    console.log(newVideo)
+    res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) =>
--- middelwares.js
+++ middelwares.js
@@ -1,4 +1,7 @@
+import multer from "multer";
 import routes from "./routes";
+
+const multerVideo = multer({ dest: "videos/" });
 
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "WeTube";
@@ -9,3 +12,5 @@ export const localsMiddleware = (req, res, next) => {
     }; //존재하지 않는 가짜 정보임 user에 객체로 정보를 가짜로 넣음
     next();
 };
+
+export const uploadVideo = multerVideo.single('videoFile');
