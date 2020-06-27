c4747a0127548b77a1d633c2f89eb4a74a4df536
kyw <[duddnr654@naver.com]>
2020-06-27 13:32:46 +0900

    3.10 Deleting a Video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -24,7 +24,7 @@ videoRouter.get(routes.editVideo(), getEditVideo);
 videoRouter.post(routes.editVideo(), postEditVideo);
 
 // Delete Video
-videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 
 export default videoRouter;
