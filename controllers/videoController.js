1ffef4b96a62aa05c7cc3b70fb24003c831c70ef
kyw <[duddnr654@naver.com]>
2020-06-27 15:07:17 +0900

    3.12 Searching Videos

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -21,13 +21,22 @@ export const home = async (req, res) => {
 
 // Search
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const {
     query: { term: searchingBy },
   } = req;
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: { $regex: searchingBy, $options: "i" },
+    });
+  } catch (error) {
+    console.log(error);
+  }
   res.render("search", {
     pageTitle: "Search",
     searchingBy,
+    videos,
   });
 };
 
