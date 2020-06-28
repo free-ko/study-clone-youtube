cc271234030094c9bd22807e72b14f0bf05f6897
kyw <[duddnr654@naver.com]>
2020-06-28 16:11:48 +0900

    4.3 ES6 with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,18 @@ const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 const config = {
-  entry: ENTRY_FILE,
+  entry: ["@babel/polyfill", ENTRY_FILE],
   mode: MODE,
   module: {
     rules: [
+      {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
       {
         test: /\.(scss)$/,
         use: ExtractCSS.extract([
@@ -19,7 +27,7 @@ const config = {
           {
             loader: "postcss-loader",
             options: {
-              plugin() {
+              plugins() {
                 return [autoprefixer({ browsers: "cover 99.5%" })];
               },
             },
