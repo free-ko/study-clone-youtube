22448f3332a32b97c80d5157ab218b1b3eb68ad2
kyw <[duddnr654@naver.com]>
2020-06-28 15:02:56 +0900

    4.2 Styles with Webpack part Two

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,41 @@
 const path = require("path");
+const autoprefixer = require("autoprefixer");
 const ExtractCSS = require("extract-text-webpack-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
-
 const config = {
   entry: ENTRY_FILE,
   mode: MODE,
   module: {
     rules: [
       {
-        test: /\.(cscss)$/,
-        use: ExtractCSS,extract([
+        test: /\.(scss)$/,
+        use: ExtractCSS.extract([
           {
-            loader: "css-loader"
+            loader: "css-loader",
           },
           {
-            loader: "postcss-loader"
+            loader: "postcss-loader",
+            options: {
+              plugin() {
+                return [autoprefixer({ browsers: "cover 99.5%" })];
+              },
+            },
           },
           {
-            loader: "sass-loader" 
-          }
-        ])
+            loader: "sass-loader",
+          },
+        ]),
       },
     ],
   },
   output: {
     path: OUTPUT_DIR,
-    filename: "[name].[format]",
+    filename: "[name].js",
   },
+  plugins: [new ExtractCSS("styles.css")],
 };
 
 module.exports = config;
