fc07bec3df3a2b358a21b3124d2ba016b325639b
kyw <[duddnr654@naver.com]>
2020-07-01 17:15:35 +0900

    5.2 Everthing else!

--- middelwares.js
+++ middelwares.js
@@ -7,7 +7,7 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
   res.locals.user = {
-    isAuthenticated: true,
+    isAuthenticated: false,
     id: 1,
   }; // 존재하지 않는 가짜 정보임 user에 객체로 정보를 가짜로 넣음
   next();
