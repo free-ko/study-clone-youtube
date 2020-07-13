8c708cf454734fbc711f8a4f19b13821da12a15d
kyw <[duddnr654@naver.com]>
2020-07-13 16:29:39 +0900

    노마드 답변 적용

--- passport.js
+++ passport.js
@@ -17,5 +17,9 @@ passport.use(
   )
 );
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
