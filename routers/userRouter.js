40421d33add8a33c7bcd2e7f8b886655821e5c93
kyw <[duddnr654@naver.com]>
2020-07-14 13:53:41 +0900

    7.2 Change Password - no study

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,17 +3,19 @@ import routes from "../routes";
 import {
   userDetail,
   getEditProfile,
-  changePassword,
+  getChangePassword,
   postEditProfile,
+  postChangePassword,
 } from "../controllers/userController";
-import { onlyPrivate } from "../middlewares";
+import { onlyPrivate, uploadAvatar } from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar, postEditProfile);
 
-userRouter.get(routes.changePassword, onlyPrivate, changePassword);
+userRouter.get(routes.changePassword, onlyPrivate, getChangePassword);
+userRouter.post(routes.changePassword, onlyPrivate, postChangePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
